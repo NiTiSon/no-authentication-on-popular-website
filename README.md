21a9d5d935e2f9c49a1feee1040a362ac7a5ea31
NiTiSon <NiTiSon@users.noreply.github.com>
2026-03-14 23:42:25 +0300

    Added other ph url

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <p align="left"><img src="logo.svg" height="120px"></p>
 
 # No authentication in popular website 
+
+> [!INFO]
+> This is a fork of [this](https://github.com/andreyvpng/no-authentication-in-popular-website) repository.
+> That repository has no LICENSE as well this.
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var selector = '';
+let selector = "";
 
-selector += '#age-verification-container';
-selector += ', .age-verification-modal';
-selector += ', #age-verification-wrapper';
+selector += "#age-verification-container";
+selector += ", .age-verification-modal";
+selector += ", #age-verification-wrapper";
 
-var elements = document.querySelectorAll(selector);
+let elements = document.querySelectorAll(selector);
 
-for (var i = 0; i < elements.length; i++) {
+for (let i = 0; i < elements.length; i++) {
   elements[i].remove();
 }
